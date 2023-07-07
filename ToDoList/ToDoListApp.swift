96cba9678d4313642f4d14d3c1bc335a9e8308b6
Gustavo Marques <gtooliveiramarques@gmail.com>
2023-07-07 16:07:34 -0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Gustavo Marques on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
